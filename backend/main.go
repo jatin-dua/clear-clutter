9b6fba4e577277da2a14e3e720523294f7b2949c
Jatin Dua <jdua004@gmail.com>
2023-07-05 17:45:41 +0530

    feat: Add router

---
+++ backend/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jatin-dua/clear-clutter/backend/router"
+)
+
+func main() {
+	router.Setup()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jatin-dua/clear-clutter
+
+go 1.20
+
+require github.com/go-chi/chi v1.5.4
