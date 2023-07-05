24d7728903a895ec95c45b95836dbb2a5408955a
Jatin Dua <jdua004@gmail.com>
2023-07-05 18:57:55 +0530

    feat: Add adb connection

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+
 	"github.com/jatin-dua/clear-clutter/backend/router"
+	adb "github.com/zach-klippenstein/goadb"
 )
 
 func main() {
+	port := flag.Int("p", adb.AdbPort, "")
+	flag.Parse()
+
+	client, err := adb.NewWithConfig(adb.ServerConfig{
+		Port: *port,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.StartServer()
+
+	serverVersion, err := client.ServerVersion()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server version:", serverVersion)
+
+	devices, err := client.ListDevices()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(devices) == 0 {
+		log.Fatal("No device connected.")
+	}
+
+	fmt.Println("Devices:")
+	for _, device := range devices {
+		fmt.Printf("\t%+v\n", *device)
+	}
+
 	router.Setup()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/jatin-dua/clear-clutter
 
 go 1.20
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/zach-klippenstein/goadb v0.0.0-20201208042340-620e0e950ed7
+)
+
+require (
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
